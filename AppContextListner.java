109011e552bebdb9e5e57401dd88816b1455c610
Naganjali11 <naganjali425@gmail.com>
2025-11-26 15:06:04 +0530

    javafullstackprograms

---
+++ AppContextListner.java
@@ -0,0 +1,14 @@
+package com.codegnan.web;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.ServletContextListener;
+
+public class AppContextListner implements ServletContextListener{
+	public void contextInitialized(ServletContextEvent sec) {
+		ServletContext context=sec.getServletContext();
+		context.setInitParameter("a","AAA");
+		context.setInitParameter("b","BBB");
+	}
+
+}
